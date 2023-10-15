9b1e3c3f367bd46dbc4f88150121ed27be55eedf
Dramos02 <ramosdominic0420@gmail.com>
2023-10-16 06:10:38 +0800

    14th Commit - Car Price List initialization

---
+++ app/src/main/java/com/example/carsey_app/Pricelist.kt
@@ -0,0 +1,11 @@
+package com.example.carsey_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Pricelist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pricelist)
+    }
+}
